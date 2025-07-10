e9809a4171084c018d33e2fbf93d8aa275cf4092
David Falk <spreetin@protonmail.com>
2025-07-10 18:53:25 +0200

    More nix fiddling

--- nix/gui.nix
+++ nix/gui.nix
@@ -4,6 +4,9 @@ let
     common = import ./common.nix { inherit pkgs; };
 in
     pkgs.stdenv.mkDerivation (common // {
+        cmakeFlags = common.cmakeFlags ++ [
+            "-DBUILD_GUI_APP=ON"
+        ];
         buildInputs = common.buildInputs 
                     ++ common.guiBuildInputs;
         buildPhase = ''
