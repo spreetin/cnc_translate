9d9f75a872352919aabec110e7dc396df4c7c311
David Falk <spreetin@protonmail.com>
2025-07-14 10:52:33 +0200

    Updated interface

--- libcnc_parse.cpp
+++ libcnc_parse.cpp
@@ -1,33 +1,40 @@
-#include <string_view>
 #include "generator/generator.h"
 #include "parser/parser.h"
+#include <string_view>
 
 namespace NCParser {
 
-parse_node_p parse(Manufacturers man, std::string_view machine, std::string text){
-    parser * p = new parser(man, machine);
-    p->parse(text);
-    auto r = p->result();
-    return r;
+parse_node_p parse(Manufacturers man, std::string_view machine,
+                   std::string text) {
+  parser p = parser(man, machine);
+  p.parse(text);
+  auto r = p.result();
+  return r;
 }
 
-parse_node_p parse(MachineParameters param, std::string text){
-    parser * p = new parser(param);
-    p->parse(text);
-    auto r = p->result();
-    return r;
+parse_node_p parse(MachineParameters &param, std::string text) {
+  parser p = parser(param);
+  p.parse(text);
+  auto r = p.result();
+  return r;
 }
 
-std::string generate(Manufacturers man, std::string_view machine, parse_node_p nodes){
-    generator * g = new generator(man, machine);
-    auto r = g->generate(nodes);
-    return r;
+std::string generate(Manufacturers man, std::string_view machine,
+                     parse_node_p nodes) {
+  generator g = generator(man, machine);
+  auto r = g.generate(nodes);
+  return r;
 }
 
-std::string generate(MachineParameters param, parse_node_p nodes){
-    generator * g = new generator(param);
-    auto r = g->generate(nodes);
-    return r;
+std::string generate(MachineParameters &param, parse_node_p nodes) {
+  generator g = generator(param);
+  auto r = g.generate(nodes);
+  return r;
 }
 
+std::string printParseTree(MachineParameters &param, parse_node_p rootNode) {
+  generator g = generator(param);
+  return g.printTree(rootNode);
 }
+
+} // namespace NCParser
--- libcnc_parse.h
+++ libcnc_parse.h
@@ -1,14 +1,17 @@
 #include <cnc_translate/brands/brands.h>
-#include <cnc_translate/parser/parse_node.h>
 #include <cnc_translate/brands/parameters.h>
-#include <vector>
+#include <cnc_translate/parser/parse_node.h>
 #include <string_view>
+#include <vector>
 
 namespace NCParser {
 
-parse_node_p parse(Manufacturers man, std::string_view machine, std::string text);
-parse_node_p parse(MachineParameters param, std::string text);
-std::string generate(Manufacturers man, std::string_view machine, parse_node_p nodes);
-std::string generate(MachineParameters param, parse_node_p nodes);
+parse_node_p parse(Manufacturers man, std::string_view machine,
+                   std::string text);
+parse_node_p parse(MachineParameters &param, std::string text);
+std::string generate(Manufacturers man, std::string_view machine,
+                     parse_node_p nodes);
+std::string generate(MachineParameters &param, parse_node_p nodes);
+std::string printParseTree(MachineParameters &param, parse_node_p rootNode);
 
-}
+} // namespace NCParser
